b0d2df7e4da6c6f2b5bb03df7586c5b808c92819
Alex Seeley <alexseeley134@gmail.com>
2023-10-17 23:01:08 -0600

    Add password match authentication and fix email
    I modified the js file and the css file to make sure the user knows
    when their password isn't matching.
    
    I also fixed the email's validation error styling so that the border
    shadow only appears when the input is focused and invalid at the same
    time.

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-window.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("form").addEventListener("submit", function () {
-    console.log("hello!");
-  });
+// Password Match Authentication
+
+const pwd = document.querySelector("#password");
+const pwdConfirm = document.querySelector("#confirm-password");
+
+let confirm1 = "";
+let confirm2 = "";
+
+function addError() {
+  let element = document.getElementById("error");
+  element.classList.remove("no-match-invisible");
+  element.classList.add("no-match-visible");
+}
+
+function removeError() {
+  let element = document.getElementById("error");
+  element.classList.remove("no-match-visible");
+  element.classList.add("no-match-invisible");
+}
+
+pwd.addEventListener("keyup", () => {
+  let pwdString = pwd.value.toString();
+  confirm1 = pwdString;
+  if (confirm1 === confirm2) {
+    removeError();
+  } else {
+    addError();
+  }
+});
+
+pwdConfirm.addEventListener("keyup", () => {
+  let pwdConfirmString = pwdConfirm.value.toString();
+  confirm2 = pwdConfirmString;
+  if (confirm1 === confirm2) {
+    removeError();
+  } else {
+    addError();
+  }
 });
